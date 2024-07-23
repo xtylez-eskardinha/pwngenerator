a66b6ab2ad9ba8da8911e283ed23ed21a108640d
xtylez-eskardinha <92118809+xtylez-eskardinha@users.noreply.github.com>
2024-07-23 00:18:47 -0600

    improve: Vulnerability database

--- src/code/vulnerable.c
+++ src/code/vulnerable.c
@@ -3,30 +3,39 @@
 #include <string.h>
 #include <stdarg.h>
 
-void input_gets_bof() {
-    char buffer[64];
-    gets(buffer);
-    return buffer;
+void ret2win(){
+    system("/bin/sh");
 }
 
-void input_gets_bof_canary() {
+void input_gets_bof(char *buf) {
+    char buffer[64];
+    gets(buffer);
+    strcpy(buf, buffer);
+}
+
+void input_gets_bof_canary(char *buf) {
     int canary = 0x20;
     char buffer[64];
     gets(buffer);
     if (canary != 0x20)
         exit(127);
-    return buffer;
+    strcpy(buf, buffer);
 }
 
-void input_strcpy_bof() {
+void input_scanf_bof(char *buf) {
+    char buffer[64];
+    scanf("%s", buffer);
+    strcpy(buf, buffer);
+}
+
+void input_strcpy_bof(char *buf) {
     char buffer[24];
     char buffer2[64];
     fgets(buffer2, sizeof(buffer2), stdin);
     strcpy(buffer, buffer2);
-    return buffer;
 }
 
-void input_strcpy_bof_canary() {
+void input_strcpy_bof_canary(char *buf) {
     int canary = 42;
     char buffer[32];
     char buffer3[64];
@@ -35,4 +44,18 @@ void input_strcpy_bof_canary() {
     if (canary != 42)
         exit(127);
     return buffer;
+}
+
+void append_input_bof(char *buf) {
+    char buffer[64];
+    scanf("%s", buffer);
+    strcat(buf, buffer);
+}
+
+void leak_stack_printf(char *buf) {
+    printf(buf);
+}
+
+void leak_stack_sprintf(char *buf, char *dest) {
+    sprintf(dest, buf);
 }
