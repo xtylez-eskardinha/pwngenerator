5c072854e81746eaf9f692ee9bf61c3dea6d6a14
xtylez-eskardinha <92118809+xtylez-eskardinha@users.noreply.github.com>
2024-07-23 00:17:53 -0600

    improve: Mini example of babybof

--- src/code/babybof.c
+++ src/code/babybof.c
@@ -3,28 +3,10 @@
 #include <string.h>
 #include <stdarg.h>
 
-void execme(){
-	system("/bin/bash");
-}
-
-void get_input(char *buffer[]) {
-    int canary = 42;
-    char buffer3[64];
-    char buffer2[24];
-    scanf("%48s", buffer2); // Sustituir por get_input()
-    printf("You've entered: %s\n", buffer2);
-    strcpy(buffer2, buffer);
-    if (canary != 42)
-        exit(127);
-}
-
-void pwnable(){
-   return 0;
-}
-
 int main(){
     printf("Tell me something and I'll answer you: ");
     char buffer[24];
-    get_input(&buffer);
+    fgets(stdin, sizeof(buffer), buffer);
+    puts(buffer);
     return 0;
 }
