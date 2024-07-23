9925004423cb1c633c00faefd53061e7edb4155d
xtylez-eskardinha <92118809+xtylez-eskardinha@users.noreply.github.com>
2024-07-23 00:16:08 -0600

    add: CLI (Beta testing)

--- src/pwngen/cmd/cli.py
+++ src/pwngen/cmd/cli.py
@@ -1,4 +1,7 @@
 from pwngen.cmd.args import Args
+from pwngen.logic.vulngen import VulnGen
+from pwngen.parsers.ast import AstProcessor
+from pwngen.pwn.exploit import Exploit
 from argparse import Namespace
 from typing import Any
 import random
@@ -9,7 +12,6 @@ class CLI:
     def __init__(self):
         self._args = self._parse_args(Args().parse_args())
         self._proc_args()
-        self.cli(interactive=self._args['interactive'])
         
     def _parse_args(self, args: Namespace) -> dict[str, Any]:
         return vars(args)
@@ -20,9 +22,8 @@ class CLI:
             return 0
         if self._args.get('seed'):
             random.seed(self._args['seed'])
-        
+    
     def _interactive(self) -> int:
-
         try:
             return 0
         except Exception as e:
@@ -31,13 +32,18 @@ class CLI:
 
     def _noninteractive(self) -> int:
         try:
+            ast = AstProcessor(self._args['input'])
+            generator = VulnGen(ast)
+            generator.inject_vulns()
+            ast.save_c('test.c')
+            gcc_flags = generator.get_compiler_syntax()
             return 0
         except Exception as e:
             print(e)
             return 1
 
-    def cli(self, interactive: bool = False):
-        return self._interactive() if interactive else self._noninteractive()
+    def cli(self):
+        return self._interactive() if self._args.get('interactive') else self._noninteractive()
 
 
 # if __name__ == '__main__':
