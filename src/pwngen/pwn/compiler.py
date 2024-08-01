f8777b10b0270f4064c8c8f29d68efec66a57500
xtylez-eskardinha <92118809+xtylez-eskardinha@users.noreply.github.com>
2024-07-31 23:26:56 -0600

    create: Compiler

---
+++ src/pwngen/pwn/compiler.py
@@ -0,0 +1,29 @@
+from subprocess import CalledProcessError, run as execute
+import os
+
+class Compiler():
+    _args: list[str]
+    _output: str
+    _compiler: str
+    _input: str
+
+    def __init__(self, args: list[str], c_input: str, output: str, compiler: str = "gcc"):
+        self._args = args
+        self._output = output
+        self._compiler = compiler
+        self._input = c_input
+    
+    def compile(self):
+        real_output = f"{self._output.split(".")[0]}.o"
+        all_args = self._args + [f"-o{real_output}"] + [self._input]
+        all_args.insert(0, self._compiler)
+        print(all_args)
+        try:
+            execute(
+                args=all_args,
+                check=True,
+                # env={'PATH': str(os.environ)}
+            )
+        except CalledProcessError as e:
+            print(e.stdout)
+            print(e.stderr)
